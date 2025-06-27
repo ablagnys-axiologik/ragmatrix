451588fbb7cc7b198959b193d564d93e328b19e3
Andy Blagnys <106306626+ablagnys-axiologik@users.noreply.github.com>
2025-06-27 18:12:33 +0100

    Update RagStatus.java

--- src/main/java/com/bnpp/pf/uk/comp/devops/ragmatrix/model/RagStatus.java
+++ src/main/java/com/bnpp/pf/uk/comp/devops/ragmatrix/model/RagStatus.java
@@ -1,16 +1,45 @@
 package com.bnpp.pf.uk.comp.devops.ragmatrix.model;
 
-public class RagStatus {
-    public String name;
-    public String path;
-    public AgeStats featureStats;
-    public AgeStats releaseStats;
+import java.util.List;
+import java.util.Map;
 
-    public RagStatus(String name, String path, AgeStats f, AgeStats r) {
-        this.name = name;
-        this.path = path;
-        this.featureStats = f;
-        this.releaseStats = r;
+public class RagStatus {
+
+    public String repoName;
+    public String repoPath;
+
+    public boolean gitflowCompliant;
+    public boolean masterProtected;
+    public boolean developProtected;
+
+    public AgeStats featureAge;
+    public AgeStats releaseAge;
+
+    public List<BranchStatus> branches;
+
+    public RagStatus() {}
+
+    public RagStatus(String name, String path, AgeStats featureAge, AgeStats releaseAge) {
+        this.repoName = name;
+        this.repoPath = path;
+        this.featureAge = featureAge;
+        this.releaseAge = releaseAge;
+    }
+
+    public void setGitflowCompliant(boolean compliant) {
+        this.gitflowCompliant = compliant;
+    }
+
+    public void setMasterProtected(boolean protectedMaster) {
+        this.masterProtected = protectedMaster;
+    }
+
+    public void setDevelopProtected(boolean protectedDevelop) {
+        this.developProtected = protectedDevelop;
+    }
+
+    public void setBranches(List<BranchStatus> branches) {
+        this.branches = branches;
     }
 
     public static class AgeStats {
@@ -21,5 +50,14 @@ public class RagStatus {
             this.avg = avg;
             this.p95 = p95;
         }
+    }
+
+    public static class BranchStatus {
+        public String branchName;
+        public String pipelineStatus;
+        public String pipelineUrl;
+        public Map<String, String> qualityGates;
+
+        public BranchStatus() {}
     }
 }
